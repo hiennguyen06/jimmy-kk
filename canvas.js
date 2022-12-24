9f9d4b4562cbb2e78b42279dde14829ae970b9b0
Hien Nguyen <hiennguyen1529@gmail.com>
2022-12-24 23:10:59 +1100

    testing three on safari

--- canvas.js
+++ canvas.js
@@ -1,53 +1,110 @@
+// import * as THREE from "three";
+// import { PointLight } from "three";
+// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+// const scene = new THREE.Scene();
+// const camera = new THREE.PerspectiveCamera(
+//   80,
+//   window.innerWidth / window.innerHeight,
+//   0.1,
+//   1000
+// );
+
+// const canvas = document.querySelector(".canvas");
+// const renderer = new THREE.WebGLRenderer({ canvas });
+// renderer.setClearColor(0xffffff, 0);
+// renderer.setSize(window.innerWidth, window.innerHeight / 1.75);
+
+// // Texture
+// const texture = new THREE.TextureLoader().load(
+//   "http://cdn.shopify.com/s/files/1/0248/7303/products/wrappingpaper-australiananimals-christmasv2copy.jpg?v=1658740431"
+// );
+
+// // Object
+// const geometry = new THREE.BoxGeometry(3, 2, 1);
+// const material = new THREE.MeshStandardMaterial({
+//   map: texture,
+// });
+// const cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
+
+// // Light
+// const light = new THREE.PointLight(0xffffff, 3, 100);
+// light.position.set(0, 10, 10);
+// scene.add(light);
+
+// const controls = new OrbitControls(camera, canvas);
+// camera.position.z = 5;
+
+// function animate() {
+//   requestAnimationFrame(animate);
+
+//   cube.rotation.y += 0.01;
+
+//   renderer.render(scene, camera);
+// }
+
+// animate();
+
+// window.addEventListener("resize", () => {
+//   renderer.setSize(window.innerWidth, window.innerHeight / 1.75);
+//   camera.aspect = window.innerWidth / window.innerHeight;
+//   camera.updateProjectionMatrix();
+// });
+
 import * as THREE from "three";
-import { PointLight } from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  80,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
+let camera, scene, renderer;
+let mesh;
 
-const canvas = document.querySelector(".canvas");
-const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setClearColor(0xffffff, 0);
-renderer.setSize(window.innerWidth, window.innerHeight / 1.75);
+init();
+animate();
 
-// Texture
-const texture = new THREE.TextureLoader().load(
-  "http://cdn.shopify.com/s/files/1/0248/7303/products/wrappingpaper-australiananimals-christmasv2copy.jpg?v=1658740431"
-);
+function init() {
+  camera = new THREE.PerspectiveCamera(
+    70,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.z = 400;
 
-// Object
-const geometry = new THREE.BoxBufferGeometry(3, 2, 1);
-const material = new THREE.MeshPhongMaterial({
-  map: texture,
-});
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+  scene = new THREE.Scene();
 
-// Light
-const light = new THREE.PointLight(0xffffff, 3, 100);
-light.position.set(0, 10, 10);
-scene.add(light);
+  const texture = new THREE.TextureLoader().load(
+    "http://cdn.shopify.com/s/files/1/0248/7303/products/wrappingpaper-australiananimals-christmasv2copy.jpg?v=1658740431"
+  );
 
-const controls = new OrbitControls(camera, canvas);
-camera.position.z = 5;
+  const geometry = new THREE.BoxGeometry(200, 200, 200);
+  const material = new THREE.MeshBasicMaterial({ map: texture });
+
+  mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  const canvas = document.querySelector(".canvas");
+  // const renderer = new THREE.WebGLRenderer({ canvas });
+
+  renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  //
+
+  window.addEventListener("resize", onWindowResize);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
 
 function animate() {
   requestAnimationFrame(animate);
 
-  cube.rotation.y += 0.01;
+  mesh.rotation.x += 0.005;
+  mesh.rotation.y += 0.01;
 
   renderer.render(scene, camera);
 }
-
-animate();
-
-window.addEventListener("resize", () => {
-  renderer.setSize(window.innerWidth, window.innerHeight / 1.75);
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-});
